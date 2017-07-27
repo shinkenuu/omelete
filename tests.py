f8ca6b039c88dde5eba38acd30aceee7c91ebc39
shinkenuu <natanael.delatorrejr@live.com>
2017-07-27 19:13:23 -0300

    Add the S command test. Refactor the L command test to test exception raising

--- tests.py
+++ tests.py
@@ -77,7 +77,7 @@ class TestMatrixEditor(unittest.TestCase):
         """
         for h in range(editor.height):
             for w in range(editor.width):
-                if editor.matrix[w][h] != '0':
+                if editor.matrix[h][w] != '0':
                     return False
         return True
 
@@ -100,26 +100,72 @@ class TestMatrixEditor(unittest.TestCase):
         self.assertTrue(self._verify_white_matrix(editor))
 
     def test_command_c(self):
+        """
+        Tests the C command - Clear the matrix
+        :return:
+        """
         height = 3
         width = 3
+
         editor = MatrixEditor(width=width, height=height)
         self.assertTrue(self._verify_white_matrix(editor))
 
     def test_command_l(self):
-        height = 8
+        """
+        Tests the L command - Writes a char within the matrix
+        :return:
+        """
+        def check_dot():
+            for h in range(height):
+                for w in range(width):
+                    if w == dot_x and h == dot_y:
+                        self.assertTrue(editor.matrix[h][w] == '1')
+                    else:
+                        self.assertTrue(editor.matrix[h][w] == '0')
+
         width = 5
+        height = 8
         editor = MatrixEditor(width=width, height=height)
 
         dot_x = 3
         dot_y = 2
         editor.color_point(x=dot_x, y=dot_y, color='1')
+        check_dot()
 
-        for h in range(height):
-            for w in range(width):
-                if w == dot_x and h == dot_y:
-                    self.assertTrue(editor.matrix[w][h] == '1')
+        with self.assertRaises(IndexError) as exc:
+            editor.color_point(x=dot_x, y=dot_y + height, color='F')
+        self.assertTrue('Coordinate out of bounds' == str(exc.exception))
+
+        with self.assertRaises(ValueError) as exc:
+            editor.color_point(x=dot_x, y=dot_y, color='')
+        self.assertTrue('Color param must be a single character' == str(exc.exception))
+
+    def test_command_s(self):
+        """
+        Tests the S command - Saves the matrix content to a file
+        :return
+        """
+        height = 13
+        width = 20
+        file_path = 's_command_result.txt'
+        editor = MatrixEditor(width=width, height=height)
+
+        editor.save_to_file(file_path=file_path)
+
+        with open(file_path, 'r') as file:
+            lines_amount = sum(1 for line in file)
+
+        line_content = ''.join(['0' for _ in range(width)])
+
+        with open(file_path, 'r') as file:
+            cur_line_idx = 0
+            for line in file:
+                cur_line_idx += 1
+                if cur_line_idx == lines_amount:
+                    self.assertTrue(line == line_content)
                 else:
-                    self.assertTrue(editor.matrix[w][h] == '0')
+                    self.assertTrue(line[:-1] == line_content)  # dont consider the \n char
+        self.assertTrue(height == lines_amount)
 
 
 if __name__ == '__main__':
